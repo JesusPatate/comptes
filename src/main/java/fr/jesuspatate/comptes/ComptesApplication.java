3daf3cd9679e197063c57d32fc42bd97a5bbbc8d
Jesuspatate <gg.jesuspatate@orange.fr>
2019-08-04 23:02:54 +0200

    Checkstyle configuration

--- src/main/java/fr/jesuspatate/comptes/ComptesApplication.java
+++ src/main/java/fr/jesuspatate/comptes/ComptesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ComptesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ComptesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ComptesApplication.class, args);
+    }
 
 }
